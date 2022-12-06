ce378245215ff0468efaa9409256f527ae3e1f7b
MNST <alexeyxl@gmail.com>
2022-12-06 13:31:25 +0400

    Change DAO address to zero-address

--- coreV2/dao/dao.go
+++ coreV2/dao/dao.go
@@ -6,6 +6,7 @@ import (
 
 // Commission which is subtracted from rewards and being send to DAO Address
 var (
-	Address    = types.HexToAddress("Mx7f0fc21d932f38ca9444f61703174569066cfa50")
+//	Address    = types.HexToAddress("Mx7f0fc21d932f38ca9444f61703174569066cfa50")
+	Address    = types.HexToAddress("Mx0000000000000000000000000000000000000000")
 	Commission = 10 // in %
 )
