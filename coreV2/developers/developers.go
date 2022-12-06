5b53c5e0910db46dd3bd52fa7c690c1a030d5616
MNST <alexeyxl@gmail.com>
2022-12-06 13:31:56 +0400

    Update developers.go

--- coreV2/developers/developers.go
+++ coreV2/developers/developers.go
@@ -6,6 +6,7 @@ import (
 
 // Commission which is subtracted from rewards and being send to Developers Address
 var (
-	Address    = types.HexToAddress("Mx688568d9d70c57e71d0b9de6480afb0d317f885c")
+//	Address    = types.HexToAddress("Mx688568d9d70c57e71d0b9de6480afb0d317f885c")
+	Address    = types.HexToAddress("Mx0000000000000000000000000000000000000000")
 	Commission = 10 // in %
 )
